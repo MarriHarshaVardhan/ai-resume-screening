3d4633c41a8e031e0dc206ee3e01cecdd4a5d68b
MarriHarshaVardhan <harshavardhanmarri08@gmail.com>
2026-08-26 15:42:41 +0530

    created models user.py tables

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,9 +7,17 @@ from sqlalchemy import pool
 
 from alembic import context
 
+# Make sure Python can find the 'app' package
+sys.path.append(os.getcwd())
+
+from app.core.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# Set the DB URL from our Settings class (which reads .env)
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,8 +28,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.db.models.user import Base
 
+target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
