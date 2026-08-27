36373affa36a0655d004082259fdab382b1e12e4
MarriHarshaVardhan <harshavardhanmarri08@gmail.com>
2026-08-27 17:04:03 +0530

    added logging in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,23 +3,11 @@ from fastapi import FastAPI
 from app.routes import router
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-
+logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
-
-app = FastAPI(
-    title="AI Resume Screening API",
-    version="1.0.0"
-)
-
-
+app = FastAPI(title="AI Resume Screening API",version="1.0.0")
 app.include_router(router)
-
-
 @app.get("/")
 def root():
     logger.info("Root endpoint called")
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 from app.api.login import router as login_router
 
-
 router = APIRouter()
 
 router.include_router(login_router)
